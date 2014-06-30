51e129b2c4ecc542caa6a258d578b0ebc081cfda
Stephen Pascoe <stephenpascoe@users.noreply.github.com>
2014-06-30 11:38:20 +0000

    Preparing for release 1.0pre1

--- setup.py
+++ setup.py
@@ -8,12 +8,17 @@
 from setuptools import setup, find_packages
 import sys, os
 
+# Load long_description from README.txt
+here = os.path.dirname(__file__)
+readme_txt = os.path.join(here, 'README.txt')
+long_description = '\n\n' + open(readme_txt).read()
+
 setup(name='ceda-cc',
-      version='0.1',
+      version='1.0',
       description="CEDA Conformance Checker",
-      #long_description="",
+      long_description=long_description,
       classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
         'Topic :: Scientific/Engineering',
         'Programming Language :: Python :: 2.6',
@@ -21,7 +26,7 @@ setup(name='ceda-cc',
       keywords='',
       author='Martin Juckes',
       author_email='Martin.Juckes@stfc.ac.uk',
-      #url='',
+      url='http://proj.badc.rl.ac.uk/exarch/wiki/PackageCedaccInstall',
       #download_url=''
       license='BSD',
       packages=find_packages(),
