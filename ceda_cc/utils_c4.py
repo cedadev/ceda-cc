26901008da3b083ee3bc8a4baaef949455dd496c
Martin <martinjuckes@users.noreply.github.com>
2014-07-31 08:33:00 +0000

    Added recognition of 'double' as equivalent to 'float64'

--- ceda_cc/utils_c4.py
+++ ceda_cc/utils_c4.py
@@ -350,7 +350,7 @@ class checkGlobalAttributes(checkBase):
     self.checkId = ('003','variable_type')
 
     mipType = vocabs['variable'].getAttr( varName, varGroup, 'type' )
-    thisType = {'real':'float32', 'integer':'int32', 'float':'float32' }.get( mipType, mipType )
+    thisType = {'real':'float32', 'integer':'int32', 'float':'float32', 'double':'float64' }.get( mipType, mipType )
     self.test( mipType == None or varAts[varName]['_type'] == thisType, 'Variable [%s/%s] not of type %s [%s]' % (varName,varGroup,str(thisType),varAts[varName]['_type']) )
 
     self.checkId = ('004','variable_ncattribute_present')
