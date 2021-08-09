ab7c0e356687f70e687f35f021119a02b6f58850
Martin Juckes <martin.juckes@stfc.ac.uk>
2021-08-09 15:47:25 +0100

    version increment

--- Package/ceda_cc/versionConfig.py
+++ Package/ceda_cc/versionConfig.py
@@ -1,4 +1,5 @@
 
-version="1.9.0"
-versionComment="Initial release of python 3 version of code, with support for CCMI 2022"
+version="1.9.1b0"
+versionComment_1_9_0="Initial release of python 3 version of code, with support for CCMI 2022"
+versionComment="Added support for SNAPSI"
 
