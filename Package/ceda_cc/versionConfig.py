8550205543b4378bfe0e8a1d10f9b4080ec395df
Martin Juckes <martin.juckes@stfc.ac.uk>
2021-09-27 15:24:58 +0100

    version uptick

--- Package/ceda_cc/versionConfig.py
+++ Package/ceda_cc/versionConfig.py
@@ -1,5 +1,5 @@
 
-version="1.9.1b1"
+version="1.9.2b1"
 versionComment_1_9_0="Initial release of python 3 version of code, with support for CCMI 2022"
-versionComment="Corrected file name template for CCMI 2022"
+versionComment="Allow multiple values in source_type"
 
