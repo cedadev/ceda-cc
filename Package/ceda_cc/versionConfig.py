c4797399be23a409f4eecfdda196adb76651acd5
Martin Juckes <martin.juckes@stfc.ac.uk>
2021-09-27 14:50:39 +0100

    update version

--- Package/ceda_cc/versionConfig.py
+++ Package/ceda_cc/versionConfig.py
@@ -1,5 +1,5 @@
 
-version="1.9.1b0"
+version="1.9.1b1"
 versionComment_1_9_0="Initial release of python 3 version of code, with support for CCMI 2022"
-versionComment="Added support for SNAPSI"
+versionComment="Corrected file name template for CCMI 2022"
 
