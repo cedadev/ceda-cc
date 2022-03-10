2ef4800166dd552c21e7bed9f3745ffc5309be5c
Martin Juckes <mjuckes@rslmj01.badc.rl.ac.uk>
2022-03-10 10:50:13 +0000

    increment version

--- Package/ceda_cc/versionConfig.py
+++ Package/ceda_cc/versionConfig.py
@@ -1,5 +1,5 @@
 
-version="1.9.2b1"
+version="1.9.2b2"
 versionComment_1_9_0="Initial release of python 3 version of code, with support for CCMI 2022"
 versionComment="Allow multiple values in source_type"
 
