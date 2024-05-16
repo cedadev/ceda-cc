1c3447221a450bd00a64b1f94d14844da6eda92c
Martin Juckes <mjuckes@rslmj01.badc.rl.ac.uk>
2024-05-16 14:52:33 +0100

    updating upload script

--- Package/pypi_upload.sh
+++ Package/pypi_upload.sh
@@ -3,11 +3,17 @@
 ## for local install:
 
 # sudo python setup.py install --force
+python3 setup.py sdist bdist_wheel
+
+python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python3 -m twine upload --repository-url  https://upload.pypi.org/legacy/ dist/*
+python3 -m twine upload  dist/*
 
 # compile egg etc (inc. wheel)
-python setup.py sdist bdist_wheel
+#python setup.py sdist bdist_wheel
 
-python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+#python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
 #python -m twine upload --repository-url  https://upload.pypi.org/legacy/ dist/*
 #python -m twine upload  dist/*
 #python setup.py sdist upload -r https://test.pypi.org/legacy/
